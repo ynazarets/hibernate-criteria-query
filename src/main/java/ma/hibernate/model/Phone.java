134c8919d58e41ac2dbd796e0f88c5dafd9bd318
Bohdan Chupika <chupika.bohdan@ukr.net>
2020-09-24 21:47:58 +0300

    renamed packages

---
+++ src/main/java/ma/hibernate/model/Phone.java
@@ -0,0 +1,75 @@
+package ma.hibernate.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Phone implements Cloneable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String model;
+    private String maker;
+    private String color;
+    private String os;
+    private String countryManufactured;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getMaker() {
+        return maker;
+    }
+
+    public void setMaker(String maker) {
+        this.maker = maker;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getOs() {
+        return os;
+    }
+
+    public void setOs(String os) {
+        this.os = os;
+    }
+
+    public String getCountryManufactured() {
+        return countryManufactured;
+    }
+
+    public void setCountryManufactured(String countryManufactured) {
+        this.countryManufactured = countryManufactured;
+    }
+
+    @Override
+    public Object clone() {
+        try {
+            return super.clone();
+        } catch (CloneNotSupportedException e) {
+            throw new RuntimeException("Can't make clone of " + this);
+        }
+    }
+}
