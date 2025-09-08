429ef3eab16d149ad32e05ae696186f0eddf4272
Yehor Nazarets <nazarets1111@gmail.com>
2025-09-08 17:11:59 +0200

    check pls

--- src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
+++ src/main/java/ma/hibernate/dao/PhoneDaoImpl.java
@@ -1,9 +1,16 @@
 package ma.hibernate.dao;
 
+import jakarta.persistence.criteria.CriteriaBuilder;
+import jakarta.persistence.criteria.CriteriaQuery;
+import jakarta.persistence.criteria.Predicate;
+import jakarta.persistence.criteria.Root;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import ma.hibernate.model.Phone;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
 public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
     public PhoneDaoImpl(SessionFactory sessionFactory) {
@@ -12,11 +19,43 @@ public class PhoneDaoImpl extends AbstractDao implements PhoneDao {
 
     @Override
     public Phone create(Phone phone) {
-        return null;
+        Transaction transaction = null;
+        try (Session session = factory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(phone);
+            transaction.commit();
+            return phone;
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Failed to create phone", e);
+        }
     }
 
     @Override
     public List<Phone> findAll(Map<String, String[]> params) {
-        return null;
+        try (Session session = factory.openSession()) {
+            CriteriaBuilder cb = session.getCriteriaBuilder();
+            CriteriaQuery<Phone> query = cb.createQuery(Phone.class);
+            Root<Phone> phoneRoot = query.from(Phone.class);
+
+            List<Predicate> predicates = new ArrayList<>();
+
+            for (Map.Entry<String, String[]> entry : params.entrySet()) {
+                String field = entry.getKey();
+                String[] values = entry.getValue();
+
+                Predicate[] orPredicates = new Predicate[values.length];
+                for (int i = 0; i < values.length; i++) {
+                    orPredicates[i] = cb.equal(phoneRoot.get(field), values[i]);
+                }
+                predicates.add(cb.or(orPredicates));
+            }
+
+            query.select(phoneRoot).where(predicates.toArray(new Predicate[0]));
+
+            return session.createQuery(query).getResultList();
+        }
     }
 }
